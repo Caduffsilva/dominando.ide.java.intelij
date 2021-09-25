266333caf78cfcb7eb61770025e000ffc5ef7dd6
Caduffsilva <caduffsilva@gmail.com>
2021-09-24 21:21:57 -0300

    first commit

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class Primeiroprograma {
+    public static void main(String[] args) {
+
+        gato Gato = new gato();
+
+        System.out.println(Gato);
+
+     /*int a=5;
+        int b=3;
+        System.out.println("hello world" + (a+b));*/
+    }
+}
